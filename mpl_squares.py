d126a4242a4209cc97060be61043375688991547
zmqingge <zmqingge@163.com>
2022-10-13 00:10:56 +0800

    校正图形&使用内置样式

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use("Solarize_Light2")
 fig,ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 #设置图表标题并给坐标轴加上标签。
 ax.set_title("square", fontsize=24)
