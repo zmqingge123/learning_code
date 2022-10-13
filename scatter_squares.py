1efddde79b149d773a576fc04dab430dfd0dea1d
zmqingge123 <zmqingge123@163.com>
2022-10-13 23:17:01 +0800

    使用scatter()绘制散点图并设置样式，并保存图片

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,12 @@
+from re import X
 import matplotlib.pyplot as plt
 
-x_values=[1, 2, 3, 4, 5]
-y_values =[1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("_classic_test_patch")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #设置图标标题并给坐标轴加上标签。
 ax.set_title("square", fontsize=24)
@@ -15,4 +16,7 @@ ax.set_ylabel("value of square", fontsize=14)
 #s设置刻度标记的大小。
 ax.tick_params(axis="both", which="major", labelsize=14)
 
-plt.show()
+#设置每个坐标轴的取值范围。
+ax.axis([0, 1100, 0, 1100000])
+
+plt.savefig("squares_plot.jpg", bbox_inches="tight")
