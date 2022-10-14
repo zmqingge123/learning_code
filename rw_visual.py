59fd79f373b246755ad232f6ce6ddcf4dbdc60e1
zmqingge123 <zmqingge123@163.com>
2022-10-14 22:39:22 +0800

    绘制随机漫步图

---
+++ rw_visual.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+#创建一个RandomWalk实例。
+rw = RandomWalk()
+rw.fill_walk()
+#将所有点都绘制出来。
+plt.style.use("classic")
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show
+
+
